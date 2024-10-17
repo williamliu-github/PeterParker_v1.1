457a1cb2f4cc97e7eecacd1b451e13a7d02e93b3
Scott Chang <scggyy1226@gmail.com>
2024-10-17 13:12:03 +0800

    Initial commit

---
+++ src/test/java/com/tibame/peterparker/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tibame.peterparker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
