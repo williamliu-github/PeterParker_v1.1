081142bad5054410fc6ecbd819528f31b4b4f300
Boyan <boyanw81@gmail.com>
2024-10-29 17:31:22 +0800

    test

--- backend/src/main/java/com/tibame/peterparker/entity/AdminVO.java
+++ backend/src/main/java/com/tibame/peterparker/entity/AdminVO.java
@@ -26,6 +26,9 @@ public class AdminVO {
     @Column(name = "admin_name")  // 對應 admin_name 列
     private String adminName;
     
+    private int test;
+    
+    
     @Column(name = "admin_status")  // 對應 admin_status 列
     private String adminStatus;
     
